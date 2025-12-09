cf3d53af1f6a4c98bbac27cda85648c18de64a37
Eric Olson <eric@cos.io>
2025-12-09 12:02:44 -0500

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ import streamlit as st
 # Basic config
 # ---------------------------------------------------------
 
-DATA_FILE = "osf_institution_dashboard.csv"
+DATA_FILE = "osfi_dashboard_data_with_summary_and_branding.csv"
 
 st.set_page_config(
     page_title="OSF Institutions Dashboard (Demo)",
